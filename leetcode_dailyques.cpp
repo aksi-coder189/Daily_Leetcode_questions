1f634a3e9305a463f63e9d37ff5912703c822919
Aakriti singh <aakritisingh1089@gmail.com>
2026-04-23 09:18:27 +0530

    Leetcode Daily Question : 23 April 2026
    Question  2615 : Sum of Distances

--- leetcode_dailyques.cpp
+++ leetcode_dailyques.cpp
@@ -1,76 +1,67 @@
-// 1722: Minimize Hamming Distance After Swap Operations
+// 2615 : Sum of distances 
+// You are given a 0-indexed integer array nums. There exists an array arr of length nums.length, where arr[i] is the sum of |i - j| over all j such that nums[j] == nums[i] and j != i. If there is no such j, set arr[i] to be 0.
+// Return the array arr.
 
-// You are given two integer arrays, source and target, both of length n. You are also given an array allowedSwaps where each allowedSwaps[i] = [ai, bi] indicates that you are allowed to swap the elements at index ai and index bi (0-indexed) of array source. Note that you can swap elements at a specific pair of indices multiple times and in any order.
-
-//The Hamming distance of two arrays of the same length, source and target, is the number of positions where the elements are different. Formally, it is the number of indices i for 0 <= i <= n-1 where source[i] != target[i] (0-indexed).
-
-// Return the minimum Hamming distance of source and target after performing any amount of swap operations on array source
 
 #include <iostream>
 #include <vector>
-#include <numeric>
 #include <unordered_map>
 
 using namespace std;
 
-class DSU {
-public:
-    vector<int> parent;
-    DSU(int n) {
-        parent.resize(n);
-        iota(parent.begin(), parent.end(), 0); // parent[i] = i
-    }
-    int find(int i) {
-        if (parent[i] == i) return i;
-        return parent[i] = find(parent[i]); // Path compression
-    }
-    void unite(int i, int j) {
-        int root_i = find(i);
-        int root_j = find(j);
-        if (root_i != root_j) parent[root_i] = root_j;
-    }
-};
-
 class Solution {
 public:
-    int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
-        int n = source.size();
-        DSU dsu(n);
-        
-        for (const auto& swap : allowedSwaps) {
-            dsu.unite(swap[0], swap[1]);
-        }
-        
-        // connecting each element
-        unordered_map<int, unordered_map<int, int>> componentCounts;
+    vector<long long> distance(vector<int>& nums) {
+        int n = nums.size();
+        vector<long long> res(n, 0);
+        unordered_map<int, vector<int>> posMap;
+
+        // 1. Group indices by their values
         for (int i = 0; i < n; ++i) {
-            int root = dsu.find(i);
-            componentCounts[root][source[i]]++;
+            posMap[nums[i]].push_back(i);
         }
-        
-        int matches = 0;
-        for (int i = 0; i < n; ++i) {
-            int root = dsu.find(i);
-            if (componentCounts[root][target[i]] > 0) {
-                matches++;
-                componentCounts[root][target[i]]--;
+
+        // 2. Process each group of indices
+        for (auto& entry : posMap) {
+            vector<int>& indices = entry.second;
+            long long m = indices.size();
+            
+            // Total sum of all indices in this group
+            long long totalSum = 0;
+            for (int idx : indices) totalSum += idx;
+
+            long long leftSum = 0;
+            for (int i = 0; i < m; ++i) {
+                long long currentIdx = indices[i];
+                long long rightSum = totalSum - leftSum - currentIdx;
+
+                // Distance from left: (count * currentIdx) - leftSum
+                long long leftDist = (long long)i * currentIdx - leftSum;
+                
+                // Distance from right: rightSum - (count * currentIdx)
+                long long rightDist = rightSum - (long long)(m - 1 - i) * currentIdx;
+
+                res[currentIdx] = leftDist + rightDist;
+
+                // Update leftSum for the next element in the group
+                leftSum += currentIdx;
             }
         }
-        
-        return n - matches;
+        return res;
     }
 };
 
 int main() {
     Solution sol;
+    vector<int> nums = {1, 3, 1, 1, 2};
+    
+    vector<long long> result = sol.distance(nums);
 
-    vector<int> source = {1, 2, 3, 4};
-    vector<int> target = {2, 1, 4, 5};
-    vector<vector<int>> allowedSwaps = {{0, 1}, {2, 3}};
-
-    int result = sol.minimumHammingDistance(source, target, allowedSwaps);
-
-    cout << "Minimum Hamming Distance: " << result << endl;
+    cout << "Sum of Distances: ";
+    for (long long x : result) {
+        cout << x << " ";
+    }
+    cout << endl;
 
     return 0;
 }
