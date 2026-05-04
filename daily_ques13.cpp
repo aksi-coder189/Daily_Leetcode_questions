7b6489147e4c72534da662c0fc8bf99e48df83e6
Aakriti singh <aakritisingh1089@gmail.com>
2026-05-04 23:52:46 +0530

    Leetcode Daily Question : 4 May 2026
    Q : 48 - rotate image

---
+++ daily_ques13.cpp
@@ -0,0 +1,56 @@
+// 48 - rotate image
+
+// You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+// You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        // Step 2: Reverse each row
+        for (int i = 0; i < n; i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
+
+void printMatrix(const vector<vector<int>>& matrix) {
+    for (const auto& row : matrix) {
+        for (int val : row) cout << val << " ";
+        cout << endl;
+    }
+}
+
+int main() {
+    Solution sol;
+    vector<vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    cout << "Original Matrix:" << endl;
+    printMatrix(matrix);
+
+    sol.rotate(matrix);
+
+    cout << "\nRotated Matrix (90 deg clockwise):" << endl;
+    printMatrix(matrix);
+
+    return 0;
+}
