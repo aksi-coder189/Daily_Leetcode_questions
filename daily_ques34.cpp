7b2c9fd97bb5a3cc2c77569dd013f5591313af4d
Aakriti singh <aakritisingh1089@gmail.com>
2026-05-25 21:42:02 +0530

    Leetcode_daily_ques : 25 May 2026
    Implement canReach function in Solution class

---
+++ daily_ques34.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        int n = s.length();
+        if (s[n - 1] == '1') return false;
+
+        vector<bool> dp(n, false);
+        dp[0] = true;
+        int reachableCount = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (i >= minJump && dp[i - minJump]) {
+                reachableCount++;
+            }
+            if (i > maxJump && dp[i - maxJump - 1]) {
+                reachableCount--;
+            }
+
+            if (s[i] == '0' && reachableCount > 0) {
+                dp[i] = true;
+            }
+        }
+
+        return dp[n - 1];
+    }
+};
