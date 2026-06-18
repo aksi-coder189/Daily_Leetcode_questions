4b26bc9f4aef1868a2eafde3b476b3c5c50763cd
Aakriti singh <aakritisingh1089@gmail.com>
2026-06-18 13:34:59 +0530

    Leetcode_daily_ques : 18 June 2026

---
+++ daily_ques58.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+        double ans = abs((30*hour)-(5.5*minutes));
+        if(ans>180) ans= 360-ans;
+        return ans;
+    }
+};
